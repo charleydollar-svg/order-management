ad903e459db15ff6b16a50e7b9e95ac8256a911d
Charles <cholzschuher@usf.edu>
2026-02-06 21:20:18 -0500

    fixed $ problem

--- cc_3b.js
+++ cc_3b.js
@@ -1,21 +1,21 @@
 let inventory = [
-    {sku: 101, name: "apple", price: 2.50, stock: 500},
-    {sku: 102, name: "banana", price: 1.25, stock: 1000},
-    {sku: 103, name: "orange", price: 1.75, stock: 750},
-    {sku: 104, name: "grape", price: 3.00, stock: 250}
+    {SKU: 101, name: "apple", price: 2.50, stock: 500},
+    {SKU: 102, name: "banana", price: 1.25, stock: 1000},
+    {SKU: 103, name: "orange", price: 1.75, stock: 750},
+    {SKU: 104, name: "grape", price: 3.00, stock: 250}
 ]
 
 inventory.forEach(item => {
-    console.log(`SKU: ${item.sku}, Name: ${item.name}, Price: $${item.price.toFixed(2)}, Stock: ${item.stock}`);
+    console.log(`SKU: ${item.SKU}, Name: ${item.name}, Price: $${item.price.toFixed(2)}, Stock: ${item.stock}`);
 })
 
-inventory.push({sku: 105, name: "pear", price: 2.00, stock: 600});
+inventory.push({SKU: 105, name: "pear", price: `$2.00`, stock: 600});
 
 let removedItem = inventory.pop();
 console.log("Removed item:", removedItem);
-console.log("Updated inventory:", inventory);
 
-inventory[1].price = 1.00;
+inventory[1].price = `$1.00`;
 console.log("bananas on sale, 20% off:", inventory[1].price);
 inventory[2].stock += 200;
 console.log("Oranges restocked to:", inventory[2].stock);
+
